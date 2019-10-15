f6de73e335cdf65d0444122eb0c0e56a23f01fd4
0emurphy <31480914+0emurphy@users.noreply.github.com>
2019-10-15 10:54:58 -0500

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -1,1 +1,14 @@
-
+import random
+def part1(x):
+  vowels = int(x)
+  consonants = 9-int(x)
+  character = 0
+  word = []
+  for i in range(vowels):
+    character = random.choice(['a','e','i','o','u',])
+    word.append(character)
+  for i in range(consonants):
+    character = random.choice(['b', 'c', 'd','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'])
+    word.append(character)
+  print(word)
+part1(6)
