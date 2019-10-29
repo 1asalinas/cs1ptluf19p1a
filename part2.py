a347e6e03068b98f92e0cdf94e7d3e69e39f7e2a
0emurphy <31480914+0emurphy@users.noreply.github.com>
2019-10-29 10:59:47 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -5,3 +5,5 @@ def check_word(astring)
     dictionary = enchant.Dict("en_US")
 
     dictionary.check(astring)
+    
+#source of import https://stackoverflow.com/questions/3788870/how-to-check-if-a-word-is-an-english-word-with-python
